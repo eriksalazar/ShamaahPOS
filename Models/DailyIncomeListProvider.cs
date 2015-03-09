8fbc6569424f8370ab867b5253bcc4e9af3923c6
eriksalazar <erik2000us@gmail.com>
2015-03-09 01:09:25 -0400

    Added format to Daily Income Index
    Added date and currency format

--- Models/DailyIncomeListProvider.cs
+++ Models/DailyIncomeListProvider.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.SqlTypes;
 using System.Linq;
 using System.Runtime.CompilerServices;
